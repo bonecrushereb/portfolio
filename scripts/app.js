59714282c87ac157973efeb3701bd9e71bbd277d
bonecrushereb <bonecrushereb@gmail.com>
2016-03-11 10:32:52 -0800

    working template

--- scripts/app.js
+++ scripts/app.js
@@ -10,33 +10,13 @@ function Projects (opts) {
 }
 
 Projects.prototype.toHtml = function() {
+
   var template = Handlebars.compile($('#project-template').html());
 
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
   this.publishedOn = this.publishedOn ? 'published' + this.daysAgo + 'days agao' : '(draft)';
 
   return template(this);
-  // var $newProjects = $('#project .template').clone();
-  // $newProjects.removeClass('template');
-  // if (!this.publishedOn) {
-  //   $newProjects.addClass('draft');
-  // }
-  // // console.log('hello there');
-  //
-  // $newProjects.find('h3:first').text(this.title);
-  // $newProjects.find('.byline a').attr('data-author', this.author);
-  // $newProjects.find('.byline a').html(this.author);
-  // $newProjects.attr('data-authorUrl', this.authorUrl);
-  // $newProjects.find('.project-body').html(this.body);
-  // $newProjects.find('time[pubdate]').attr('datetime', this.publishedOn);
-  // $newProjects.find('time[pubdate]').attr('title', this.publishedOn);
-  // $newProjects.find('.project-img img').attr('src', this.img);
-  //
-  // $newProjects.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-  //
-  // $newProjects.append('<hr>');
-  // // console.log($newProjects);
-  // return $newProjects;
 };
 
 projectData.sort(function(a,b) {
