57909cca28d489757a0f60f76de1cc3410a53238
bonecrushereb <bonecrushereb@gmail.com>
2016-03-10 22:09:15 -0800

    worked on templating projects

--- scripts/app.js
+++ scripts/app.js
@@ -10,27 +10,33 @@ function Projects (opts) {
 }
 
 Projects.prototype.toHtml = function() {
-  var $newProjects = $('#project .template').clone();
-  $newProjects.removeClass('template');
-  if (!this.publishedOn) {
-    $newProjects.addClass('draft');
-  }
-  // console.log('hello there');
+  var template = Handlebars.compile($('#project-template').html());
 
-  $newProjects.find('h3:first').text(this.title);
-  $newProjects.find('.byline a').attr('data-author', this.author);
-  $newProjects.find('.byline a').html(this.author);
-  $newProjects.attr('data-authorUrl', this.authorUrl);
-  $newProjects.find('.project-body').html(this.body);
-  $newProjects.find('time[pubdate]').attr('datetime', this.publishedOn);
-  $newProjects.find('time[pubdate]').attr('title', this.publishedOn);
-  $newProjects.find('.project-img img').attr('src', this.img);
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.publishedOn = this.publishedOn ? 'published' + this.daysAgo + 'days agao' : '(draft)';
 
-  $newProjects.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-
-  $newProjects.append('<hr>');
-  // console.log($newProjects);
-  return $newProjects;
+  return template(this);
+  // var $newProjects = $('#project .template').clone();
+  // $newProjects.removeClass('template');
+  // if (!this.publishedOn) {
+  //   $newProjects.addClass('draft');
+  // }
+  // // console.log('hello there');
+  //
+  // $newProjects.find('h3:first').text(this.title);
+  // $newProjects.find('.byline a').attr('data-author', this.author);
+  // $newProjects.find('.byline a').html(this.author);
+  // $newProjects.attr('data-authorUrl', this.authorUrl);
+  // $newProjects.find('.project-body').html(this.body);
+  // $newProjects.find('time[pubdate]').attr('datetime', this.publishedOn);
+  // $newProjects.find('time[pubdate]').attr('title', this.publishedOn);
+  // $newProjects.find('.project-img img').attr('src', this.img);
+  //
+  // $newProjects.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+  //
+  // $newProjects.append('<hr>');
+  // // console.log($newProjects);
+  // return $newProjects;
 };
 
 projectData.sort(function(a,b) {
