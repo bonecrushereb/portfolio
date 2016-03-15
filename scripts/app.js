de3289ecf0f86b0a2ddfe1203725e8ca6856c36e
bonecrushereb <bonecrushereb@gmail.com>
2016-03-14 19:25:12 -0700

    finished responsive layout

--- scripts/app.js
+++ scripts/app.js
@@ -31,43 +31,42 @@ projects.forEach(function(a){
 
 // function skillsChartGenerator(){
 //
-//   var skills = document.getElementById('skillsChart').getContext('2d');
-//   var myNewChart = new Chart(skills).PolarArea(data);
+//   var ctx = document.getElementById("skillsChart").getContext("2d");
+//   var myNewChart = new Chart(ctx).PolarArea(data);
 //
 //   var data = [
-//       {
-//           value: 300,
-//           color:"#F7464A",
-//           highlight: "#FF5A5E",
-//           label: "Red"
-//       },
-//       {
-//           value: 50,
-//           color: "#46BFBD",
-//           highlight: "#5AD3D1",
-//           label: "Green"
-//       },
-//       {
-//           value: 100,
-//           color: "#FDB45C",
-//           highlight: "#FFC870",
-//           label: "Yellow"
-//       },
-//       {
-//           value: 40,
-//           color: "#949FB1",
-//           highlight: "#A8B3C5",
-//           label: "Grey"
-//       },
-//       {
-//           value: 120,
-//           color: "#4D5360",
-//           highlight: "#616774",
-//           label: "Dark Grey"
-//       }
+//     {
+//         value: 300,
+//         color:"#F7464A",
+//         highlight: "#FF5A5E",
+//         label: "Red"
+//     },
+//     {
+//         value: 50,
+//         color: "#46BFBD",
+//         highlight: "#5AD3D1",
+//         label: "Green"
+//     },
+//     {
+//         value: 100,
+//         color: "#FDB45C",
+//         highlight: "#FFC870",
+//         label: "Yellow"
+//     },
+//     {
+//         value: 40,
+//         color: "#949FB1",
+//         highlight: "#A8B3C5",
+//         label: "Grey"
+//     },
+//     {
+//         value: 120,
+//         color: "#4D5360",
+//         highlight: "#616774",
+//         label: "Dark Grey"
+//     }
 //
-//   ];
-//
+// ];
 // }
 //
 // skillsChartGenerator();
