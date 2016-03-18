d954929687845a2d9940a06a44387b1462face28
bonecrushereb <bonecrushereb@gmail.com>
2016-03-18 14:31:29 -0700

    moved images to images folder

--- scripts/PortfolioView.js
+++ scripts/PortfolioView.js
@@ -4,28 +4,28 @@
 
   portfolioView.handleMainNav = function() {
 
-    $('.main-nav').on('click', 'li' ,function(){
+    $('.main-nav').on('click', 'li', function() {
       var $val = $(this).attr('data-content');
       console.log($val);
       $('.tab-content').hide();
       $('main-nav').show();
-      $('.tab-content[id = "'+ $val +'"]').fadeIn();
+      $('.tab-content[id = "' + $val + '"]').fadeIn();
 
     });
 
     $('.main-nav .tab:first').click();
   };
 
-  portfolioView.hamburgerClickEvent = function(){
-      $('.icon-menu, .tab a').on('click', function() {
-        $('.main-nav ul').toggle();
-        console.log('I am functioning');
-      });
+  portfolioView.hamburgerClickEvent = function() {
+    $('.icon-menu').on('click', function() {
+      $('.main-nav ul').toggle();
+      console.log('I am functioning');
+    });
   };
 
 
   portfolioView.initIndexPage = function() {
-    Projects.all.forEach(function(a){
+    Projects.all.forEach(function(a) {
       $('#project').append(a.toHtml());
     });
 
