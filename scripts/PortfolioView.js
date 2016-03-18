d7c4c6f247cbaba495e18a0f8586bbd22e51c221
bonecrushereb <bonecrushereb@gmail.com>
2016-03-18 12:44:18 -0700

    merge conflict

--- scripts/PortfolioView.js
+++ scripts/PortfolioView.js
@@ -4,24 +4,36 @@
 
   portfolioView.handleMainNav = function() {
 
-    $('.main-nav').on('click', 'li', function() {
+    $('.main-nav').on('click', 'li' ,function(){
       var $val = $(this).attr('data-content');
       console.log($val);
       $('.tab-content').hide();
-      $('.tab-content[id = "' + $val + '"]').fadeIn();
+      $('main-nav').show();
+      $('.tab-content[id = "'+ $val +'"]').fadeIn();
 
     });
 
     $('.main-nav .tab:first').click();
   };
 
+  portfolioView.hamburgerClickEvent = function(){
+    if($('.icon-menu').hasClass('display: none')){
+
+      $('.icon-menu, .tab a').on('click', function() {
+        $('.main-nav ul').toggle();
+        console.log('I am functioning');
+      });
+    };
+  };
+
 
   portfolioView.initIndexPage = function() {
-    Projects.all.forEach(function(a) {
+    Projects.all.forEach(function(a){
       $('#project').append(a.toHtml());
     });
 
     portfolioView.handleMainNav();
+    portfolioView.hamburgerClickEvent();
   };
 
   module.portfolioView = portfolioView;
--- scripts/app.js
+++ scripts/app.js
@@ -1,28 +1,35 @@
-function Projects(opts) {
-  this.author = opts.author;
-  this.authorUrl = opts.authorUrl;
-  this.title = opts.title;
-  this.body = opts.body;
-  this.publishedOn = opts.publishedOn;
-  this.img = opts.img;
-}
+(function(module) {
+  
+  function Projects (opts) {
+    this.author = opts.author;
+    this.authorUrl = opts.authorUrl;
+    this.title = opts.title;
+    this.body = opts.body;
+    this.publishedOn = opts.publishedOn;
+    this.img = opts.img;
+  }
 
-Projects.all = [];
+  Projects.all = [];
 
-Projects.prototype.toHtml = function() {
+  Projects.prototype.toHtml = function() {
 
-  var template = Handlebars.compile($('#project-template').html());
+    var template = Handlebars.compile($('#project-template').html());
 
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000);
-  this.publishedOn = this.publishedOn ? 'published' + this.daysAgo + 'days agao' : '(draft)';
+    this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+    this.publishedOn = this.publishedOn ? 'published' + this.daysAgo + 'days agao' : '(draft)';
 
-  return template(this);
-};
+    return template(this);
+  };
 
-Projects.loadAll = function(rawData) {
-  rawData.sort(function(a, b) {
-    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-  });
+  Projects.loadAll = function(rawData) {
+    rawData.sort(function(a,b) {
+      return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+    });
+
+    rawData.forEach(function(ele) {
+      Projects.all.push(new Projects(ele));
+    });
+  };
 
   Projects.fetchAll = function() {
     console.log('fetchAll does fire!');
@@ -32,22 +39,17 @@ Projects.loadAll = function(rawData) {
       portfolioView.initIndexPage();
     } else {
       console.log('json');
-      $.getJSON('data/projectData.json', function(rawData) {
+      $.getJSON('data/projectData.json', function(rawData){
         console.log('json retreval');
         Projects.loadAll(rawData);
         localStorage.setItem('rawData', JSON.stringify(rawData));
         console.log('rawData is set');
         portfolioView.initIndexPage();
-      }).error(function(err) {
+      }).error(function (err){
         console.log(err);
       });
-    };
-
-    function hamburgerClickEvent() {
-      $('.icon-menu, .tab a').on('click', function() {
-        $('.main-nav ul').toggle();
-      });
     }
-    hamburgerClickEvent();
   };
-};
+
+  module.Projects = Projects;
+})(window);
