875b4870a4af103109257bf14eadf4bb803241f2
bonecrushereb <bonecrushereb@gmail.com>
2016-03-18 08:36:35 -0700

    trying to get nav back to functional

--- scripts/PortfolioView.js
+++ scripts/PortfolioView.js
@@ -1,19 +1,23 @@
 (function(module) {
+
   var portfolioView = {};
 
   portfolioView.handleMainNav = function() {
-    $('.main-nav').on('click', 'li', function() {
+
+    $('.main-nav').on('click', 'li' ,function(){
       var $val = $(this).attr('data-content');
       console.log($val);
       $('.tab-content').hide();
-      // $('main-nav').show();
-      $('.tab-content[id = "' + $val + '"]').fadeIn();
+      $('main-nav').show();
+      $('.tab-content[id = "'+ $val +'"]').fadeIn();
+
     });
+
     $('.main-nav .tab:first').click();
   };
 
   portfolioView.hamburgerClickEvent = function(){
-    if($('.icon-menu').hasClass('display: block')){
+    if($('.icon-menu').hasClass('display: none')){
 
       $('.icon-menu, .tab a').on('click', function() {
         $('.main-nav ul').toggle();
