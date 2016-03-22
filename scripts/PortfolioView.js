132e1be07167e782c147237deb3994efd4206752
bonecrushereb <bonecrushereb@gmail.com>
2016-03-21 20:50:43 -0700

    removed mainNav function

--- scripts/PortfolioView.js
+++ scripts/PortfolioView.js
@@ -2,19 +2,19 @@
 
   var portfolioView = {};
 
-  portfolioView.handleMainNav = function() {
-
-    $('.main-nav').on('click', 'li', function() {
-      var $val = $(this).attr('data-content');
-      console.log($val);
-      $('.tab-content').hide();
-      $('main-nav').show();
-      $('.tab-content[id = "' + $val + '"]').fadeIn();
-
-    });
-
-    $('.main-nav .tab:first').click();
-  };
+  // portfolioView.handleMainNav = function() {
+  //
+  //   $('.main-nav').on('click', 'li', function() {
+  //     var $val = $(this).attr('data-content');
+  //     console.log($val);
+  //     $('.tab-content').hide();
+  //     $('main-nav').show();
+  //     $('.tab-content[id = "' + $val + '"]').fadeIn();
+  //
+  //   });
+  //
+  //   $('.main-nav .tab:first').click();
+  // };
 
   portfolioView.hamburgerClickEvent = function() {
     $('.icon-menu').on('click', function() {
